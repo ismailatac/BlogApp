81aec0a65907d2c6d5b43ad6c593619acda1c846
ismail <atac571@outlook.com>
2023-10-21 11:00:52 +0300

    dashboard category list add

---
+++ BlogApp/ViewComponents/Category/CategoryListDashboard.cs
@@ -0,0 +1,16 @@
+﻿using Business.Concretes;
+using DataAccess.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogApp.ViewComponents.Category
+{
+    public class CategoryListDashboard : ViewComponent
+    {
+        CategoryManager cm = new CategoryManager(new EfCategoryRepository());
+        public IViewComponentResult Invoke()
+        {
+            var values = cm.GetListAll();
+            return View(values);
+        }
+    }
+}
